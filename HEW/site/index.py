e9505a5f5bbb51b980310fb545141ab81954b561
syxnji <syunji0710@gmail.com>
2023-11-30 19:11:04 +0900

    作成者名の追加

--- HEW/site/index.py
+++ HEW/site/index.py
@@ -53,7 +53,7 @@ def TrendPage():
 # ------------------------------------------------------------
 
 # /register/
-@app.route('/register/',methods=['POST'])   #小濱俊史
+@app.route('/register/',methods=['POST'])   # 小濱俊史
 def register():
     if request.method == 'POST':
         
@@ -87,7 +87,7 @@ def register():
         return render_template("login.html")
    
 # /login/
-@app.route('/login/', methods=['POST']) #小濱俊史
+@app.route('/login/', methods=['POST']) # 小濱俊史
 def login():
     if request.method == 'POST':
         
@@ -124,7 +124,7 @@ def login():
             return render_template("login.html", PassMessage=PassMessage)
 
 # /sell/
-@app.route('/sell/', methods=['POST'])  #小濱俊史
+@app.route('/sell/', methods=['POST'])  # 小濱俊史
 def Sell():    
     if request.method == 'POST':
         you_list = session.get('you')
