c15126bcf7753dc1ff1aeb87c8c3f8d65f4c2101
Kakusuu28 <rei251223@gmail.com>
2023-12-06 22:49:28 +0900

    一旦ね

--- HEW/site/managelog.py
+++ HEW/site/managelog.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
-# db接続
+# データベース接続
 def conn_db():
     conn = mysql.connector.connect(
         host="127.0.0.1",
@@ -13,36 +13,117 @@ def conn_db():
     )
     return conn
 
-# manageuserで表示する情報をselect取り出し
+#各ページへの遷移
+@app.route('/index')
+def IndexPage():
+    return render_template("index.html")
+
+@app.route('/managetop')
+def ManagetopPage():
+    return render_template("managetop.html")
+
+@app.route('/managepage')
+def ManagePage():
+    return render_template("managepage.html")
+
+@app.route('/managedb')
+def ManagedbPage():
+    return render_template("managedb.html")
+
+# ユーザー情報を表示
 @app.route("/manageuser")
 def manageuser():
     conn = conn_db()
     cursor = conn.cursor()
-    sql = "select * from account"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return render_template("manageuser.html", result=result)
+    try:
+        sql = "SELECT * FROM account"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        return render_template("manageuser.html", result=result)
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
 
-# 削除するとき
-@app.route("/delete/", methods=["GET"])
-def user_delete():
+# 新しいユーザーをデータベースに追加
+@app.route("/adduser", methods=["POST"])
+def adduser():
     conn = None
     cursor = None
 
     try:
-        id = request.args.get("id", "")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        # 他の情報もフォームから取得
+
         conn = conn_db()
         cursor = conn.cursor()
-        # id(0の値)を消す
-        sql = "delete from account where AccountID={0};".format(id)
-        cursor.execute(sql)
+#%s:インジェクション
+        sql = "INSERT INTO account (Username, Email) VALUES (%s, %s)"
+        cursor.execute(sql, (username, email))
         conn.commit()
-    except mysql.connector.ProgrammingError as e:
+
+    except mysql.connector.Error as e:
         print(e)
     finally:
-        if cursor is not None:
+        if cursor:
             cursor.close()
-        elif conn is not None:
+        if conn:
+            conn.close()
+
+    return redirect(url_for("manageuser"))
+
+# ユーザー情報を編集
+@app.route("/edituser", methods=["POST"])
+def edituser():
+    conn = None
+    cursor = None
+
+    try:
+        user_id = request.form.get("user_id")
+        new_email = request.form.get("new_email")
+        # 他の情報もフォームから取得
+
+        conn = conn_db()
+        cursor = conn.cursor()
+
+        sql = "UPDATE account SET Email=%s WHERE AccountID=%s"
+        cursor.execute(sql, (new_email, user_id))
+        conn.commit()
+
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+    return redirect(url_for("manageuser"))
+
+# ユーザーを削除
+@app.route("/deleteuser", methods=["POST"])
+def deleteuser():
+    conn = None
+    cursor = None
+
+    try:
+        user_id = request.form.get("user_id")
+
+        conn = conn_db()
+        cursor = conn.cursor()
+
+        sql = "DELETE FROM account WHERE AccountID=%s"
+        cursor.execute(sql, (user_id,))
+        conn.commit()
+
+    except mysql.connector.Error as e:
+        print(e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
             conn.close()
 
     return redirect(url_for("manageuser"))
