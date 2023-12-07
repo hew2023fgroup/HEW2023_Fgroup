f390030a52c02356a17a83b2b7b2242cf7e07d8e
Kakusuu28 <rei251223@gmail.com>
2023-12-07 18:50:35 +0900

    管理者判別-1

--- HEW/site/managelog.py
+++ HEW/site/managelog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,session
 import mysql.connector
 
 app = Flask(__name__)
@@ -189,10 +189,51 @@ def deleteuser():
     return redirect(url_for("ManageuserPage"))
 
 
-#特定のMailAddressの場合の遷移判定/login.pyにいれたほうがいいかも
+#特定のMailAddressの場合の遷移判定
+@app.route('/login')
+def LoginPage():
+    return render_template("login.html")
+# /register
+@app.route('/register')
+def RegistrationPage():
+    return render_template("registration.html")
 
-#@app.route('/')
-#def index():
+    
+@app.route('/login/', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        
+        # フォーム
+        email = request.form['email']
+        password = request.form['password']
+        
+        # SELECT
+        conn = conn_db()
+        cursor = conn.cursor()
+        sql = "SELECT MailAddress, Password FROM Account WHERE MailAddress = '{0}' AND Password = '{1}';".format(email, password)
+        cursor.execute(sql)
+        
+        # 認証
+        user = cursor.fetchone()
+        if user:
+            # 成功
+            
+            # ↓8行追加-出品機能のAccountID用
+            get = '''
+                   SELECT AccountID, UserName, MailAddress 
+                   FROM Account WHERE MailAddress = '{0}' AND Password = '{1}';
+                  '''.format(email, password)
+            cursor.execute(get)
+            you = cursor.fetchall()
+            # セッションへログイン情報を保存
+            session['you'] = you
+            return render_template("index.html")
+        else:
+            # 失敗
+            PassMessage = "！！メールアドレスとパスワードが一致しません！！"
+            return render_template("login.html", PassMessage=PassMessage)
+#@app.route('/login/')
+#def login():
 
 
 #    if MailAddress == 'hoge@hoge':
