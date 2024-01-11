66390da1755f1225f25da35673edb617c19146ed
syxnji <syunji0710@gmail.com>
2024-01-11 19:31:27 +0900

    displayへの表示未完成

--- HEW/site/static/js/regist_modal.js
+++ HEW/site/static/js/regist_modal.js
@@ -1,4 +1,14 @@
 $(function () {
+
+    // フォームの入力値
+    var username = document.getElementById('username').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    document.getElementById('disp_username').innerHTML = username;
+    document.getElementById('disp_email').innerHTML = email;
+    document.getElementById('disp_password').innerHTML = passwword;
+
     $('#openModal').click(function(){
         $('#modalArea').fadeIn();
     });
