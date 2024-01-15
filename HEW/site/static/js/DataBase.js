0d7e3ed14147fdd551deb6102e4fb5dfb484f97f
hikaru1309 <hikarishiro2001@icloud.com>
2024-01-15 18:22:17 +0900

    DataBase用js

--- HEW/site/static/js/DataBase.js
+++ HEW/site/static/js/DataBase.js
@@ -59,3 +59,19 @@ $(function(){
         $(this).parents('tr').remove();
     });
 });
+
+
+
+/*---------------SellIMG---------------*/
+$(function(){
+    $('tbody').sortable();
+
+    $('#addRow').click(function(){
+        var html = '<tr><td><input type="text" name="SellIMGID" id="" class="size-input-SellIMGID"></td><td><input type="text" name="SellIMG" id="" class="size-input-SellIMG"></td><td><input type="text" name="ThumbnailFlg" id="" class="size-input-ThumbnailFlg"></td><td><input type="text" name="SellID" id="" class="size-input-SellID"></td><td><button class="remove">-</button></td></tr>';
+        $('tbody').append(html);
+    });
+
+    $(document).on('click', '.remove', function() {
+        $(this).parents('tr').remove();
+    });
+});
